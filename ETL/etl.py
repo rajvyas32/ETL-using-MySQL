2cd2ee72a48ae43b1dd0f2802db3759df433441d
rajvyas32 <124141023+rajvyas32@users.noreply.github.com>
2023-02-01 13:29:25 -0700

    Add files via upload

--- ETL/etl.py
+++ ETL/etl.py
@@ -3,36 +3,45 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 #Database connection
-host='db'
-user='root'
-password='Admin123'
-db='Users'
+try:
+    host='db'
+    user='root'
+    password='Admin123'
+    db='Users'
+    engine = create_engine(f'mysql://{user}:{password}@{host}/{db}')
+    print("Connected to Users database successfully")
+except Exception as e:
+    print("The connection to Users database was not successfull")
 
-engine = create_engine(f'mysql://{user}:{password}@{host}/{db}')
 
-# Read in the CSV file
-df = pd.read_csv('SRDataEngineerChallenge_DATASET.csv')
 
-# Checking Nulls
-print("Null Values Summary: \n",df.isna().sum())
+try:
+    # Read in the CSV file
+    df = pd.read_csv('SRDataEngineerChallenge_DATASET.csv')
+    print("\nRead CSV operation succesfull \n")
 
-# Checking Duplicates
-print("\n Duplicate Values Summary:",df.duplicated().sum())
+    # Checking Nulls
+    print("Null Values Summary: \n",df.isna().sum())
 
-# Transformations
-# Concatenating First and Last Name
-df['full_name'] = df['first_name'] + " " + df['last_name']
-# Filtering rows whose email doesn't end with .com
-df = df[df['email'].str.endswith('.com')]
+    # Checking Duplicates
+    print("\nDuplicate Values Summary:",df.duplicated().sum())
 
+    # Transformations
+    # Concatenating First and Last Name
+    df['full_name'] = df['first_name'] + " " + df['last_name']
+    # Filtering rows whose email doesn't end with .com
+    df = df[df['email'].str.endswith('.com')]
+except Exception as e:
+    print("\nError in reading CSV or perfoming transformation")
 
 
 
 
 # Load the data into the database
-df.to_sql('users_info', con=engine, if_exists='replace',index=False)
+try:
+    df.to_sql('users_info', con=engine, if_exists='replace',index=False)
+    print("\nTable users_info inserted Successfully")
+    engine.dispose()
+except Exception as e:
+    print("\nUnable to load users_info table in Users database")
 
-print("Table inserted Successfully")
-
-
-engine.dispose()
