3ce05dfe7c7847f8d699346cfe02edc71cb97f8d
rajvyas32 <124141023+rajvyas32@users.noreply.github.com>
2023-02-01 13:41:43 -0700

    Add files via upload

--- ETL/etl.py
+++ ETL/etl.py
@@ -2,46 +2,49 @@ import pandas as pd
 # import pymysql
 from sqlalchemy import create_engine
 
-#Database connection
+
 try:
+    #Database connection
     host='db'
     user='root'
     password='Admin123'
     db='Users'
     engine = create_engine(f'mysql://{user}:{password}@{host}/{db}')
     print("Connected to Users database successfully")
+
+    try:
+        # Read in the CSV file
+        df = pd.read_csv('SRDataEngineerChallenge_DATASET.csv')
+        print("\nRead CSV operation succesfull \n")
+
+        # Checking Nulls
+        print("Null Values Summary: \n",df.isna().sum())
+
+        # Checking Duplicates
+        print("\nDuplicate Values Summary:",df.duplicated().sum())
+
+        # Transformations
+        # Concatenating First and Last Name
+        df['full_name'] = df['first_name'] + " " + df['last_name']
+        # Filtering rows whose email doesn't end with .com
+        df = df[df['email'].str.endswith('.com')]
+    except Exception as e:
+        print("\nError in reading CSV or perfoming transformation")
+
+
+
+    # Load the data into the database
+    try:
+        df.to_sql('users_info', con=engine, if_exists='replace',index=False)
+        print("\nTable users_info inserted Successfully")
+        engine.dispose()
+    except Exception as e:
+        print("\nUnable to load users_info table in Users database")
+
+
 except Exception as e:
     print("The connection to Users database was not successfull")
 
 
 
-try:
-    # Read in the CSV file
-    df = pd.read_csv('SRDataEngineerChallenge_DATASET.csv')
-    print("\nRead CSV operation succesfull \n")
-
-    # Checking Nulls
-    print("Null Values Summary: \n",df.isna().sum())
-
-    # Checking Duplicates
-    print("\nDuplicate Values Summary:",df.duplicated().sum())
-
-    # Transformations
-    # Concatenating First and Last Name
-    df['full_name'] = df['first_name'] + " " + df['last_name']
-    # Filtering rows whose email doesn't end with .com
-    df = df[df['email'].str.endswith('.com')]
-except Exception as e:
-    print("\nError in reading CSV or perfoming transformation")
-
-
-
-
-# Load the data into the database
-try:
-    df.to_sql('users_info', con=engine, if_exists='replace',index=False)
-    print("\nTable users_info inserted Successfully")
-    engine.dispose()
-except Exception as e:
-    print("\nUnable to load users_info table in Users database")
 
